604e19a4be38aadd19bbedff4548df7a99cda27a
Moritz Ruge <ruge.moritz@gmail.com>
2024-10-31 01:09:18 +0100

    First commit

---
+++ u02_Aufgabe_3_d.py
@@ -0,0 +1,7 @@
+#Schreiben Sie ein Python-Programm, das drei ganze Zahlen erhält, und ausgibt, um wie viele verschiedene Zahlen es sich handelt.
+
+def wieviel_verschiedene_zahlen(a,b,c):
+    if a == b and a == c and b == c:
+        print("0")
+    elif 
+
